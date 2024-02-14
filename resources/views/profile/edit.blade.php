01bdcb74b6c6f15de68640c0b7b9e00239b73732
Deivy Potosi <deibypotosi535@gmail.com>
2024-02-14 07:31:03 -0500

    Profile Incomplete

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -52,10 +52,10 @@
         <div class="container-fluid d-flex align-items-center">
             <div class="row">
                 <div class="col-lg-7 col-md-10">
-                    <h1 class="display-2 text-white">Hello Jesse</h1>
-                    <p class="text-white mt-0 mb-5">This is your profile page. You can see the progress you've made with
-                        your work and manage your projects or assigned tasks</p>
-                    <a href="#!" class="btn btn-info">Edit profile</a>
+                    <h1 class="display-2 text-white">Hola {{$user->name}} </h1>
+                    <p class="text-white mt-0 mb-5">Esta es tu página de perfil. Puedes ver el progreso que has hecho con
+                        tu trabajo y gestionar tus proyectos o tareas asignadas</p>
+                    <a href="#!" class="btn btn-info">Editar perfil</a>
                 </div>
             </div>
         </div>
@@ -70,7 +70,7 @@
                         <div class="col-lg-3 order-lg-2">
                             <div class="card-profile-image">
                                 <a href="#">
-                                    <img src="../assets/img/theme/team-4-800x800.jpg" class="rounded-circle">
+                                    <img src=" {{ asset('img/theme/team-4-800x800.jpg')}}" class="rounded-circle">
                                 </a>
                             </div>
                         </div>
@@ -85,18 +85,7 @@
                         <div class="row">
                             <div class="col">
                                 <div class="card-profile-stats d-flex justify-content-center mt-md-5">
-                                    <div>
-                                        <span class="heading">22</span>
-                                        <span class="description">Friends</span>
-                                    </div>
-                                    <div>
-                                        <span class="heading">10</span>
-                                        <span class="description">Photos</span>
-                                    </div>
-                                    <div>
-                                        <span class="heading">89</span>
-                                        <span class="description">Comments</span>
-                                    </div>
+                                    
                                 </div>
                             </div>
                         </div>
@@ -126,39 +115,40 @@
                     <div class="card-header bg-white border-0">
                         <div class="row align-items-center">
                             <div class="col-8">
-                                <h3 class="mb-0">My account</h3>
+                                <h3 class="mb-0">Mi cuenta</h3>
                             </div>
                             <div class="col-4 text-right">
-                                <a href="#!" class="btn btn-sm btn-primary">Settings</a>
+                                <a href="#!" class="btn btn-sm btn-primary">Configuraciones</a>
                             </div>
                         </div>
                     </div>
                     <div class="card-body">
                         <form>
-                            <h6 class="heading-small text-muted mb-4">User information</h6>
+                            <h6 class="heading-small text-muted mb-4">Información usuario</h6>
                             <div class="pl-lg-4">
                                 <div class="row">
                                     <div class="col-lg-6">
                                         <div class="form-group">
-                                            <label class="form-control-label" for="input-username">Username</label>
-                                            <input type="text" id="input-username"
-                                                class="form-control form-control-alternative" placeholder="Username"
-                                                value="lucky.jesse">
+                                            <label class="form-control-label" for="name">Nombre de usuario</label>
+                                            <input type="text"  class="form-control 
+                                                id="name" name="name"
+                                                form-control-alternative"
+                                                value="{{old('name', $user->name)}}" required autofocus autocomplete="name">
                                         </div>
                                     </div>
                                     <div class="col-lg-6">
                                         <div class="form-group">
-                                            <label class="form-control-label" for="input-email">Email address</label>
-                                            <input type="email" id="input-email"
+                                            <label class="form-control-label" for="email">Dirección de correo electrónico</label>
+                                            <input type="email" id="email"
                                                 class="form-control form-control-alternative"
-                                                placeholder="jesse@example.com">
+                                                value="{{ old('email', $user->email) }}" required autocomplete="name">
                                         </div>
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col-lg-6">
                                         <div class="form-group">
-                                            <label class="form-control-label" for="input-first-name">First name</label>
+                                            <label class="form-control-label" for="input-first-name">Nombre</label>
                                             <input type="text" id="input-first-name"
                                                 class="form-control form-control-alternative" placeholder="First name"
                                                 value="Lucky">
@@ -166,7 +156,7 @@
                                     </div>
                                     <div class="col-lg-6">
                                         <div class="form-group">
-                                            <label class="form-control-label" for="input-last-name">Last name</label>
+                                            <label class="form-control-label" for="input-last-name">Apellido</label>
                                             <input type="text" id="input-last-name"
                                                 class="form-control form-control-alternative" placeholder="Last name"
                                                 value="Jesse">
@@ -176,7 +166,7 @@
                             </div>
                             <hr class="my-4" />
                             <!-- Address -->
-                            <h6 class="heading-small text-muted mb-4">Contact information</h6>
+                            <h6 class="heading-small text-muted mb-4">Actualizar contraseña</h6>
                             <div class="pl-lg-4">
                                 <div class="row">
                                     <div class="col-md-12">
@@ -216,7 +206,7 @@
                             </div>
                             <hr class="my-4" />
                             <!-- Description -->
-                            <h6 class="heading-small text-muted mb-4">About me</h6>
+                            <h6 class="heading-small text-muted mb-4">Eliminar cuenta</h6>
                             <div class="pl-lg-4">
                                 <div class="form-group">
                                     <label>About Me</label>
